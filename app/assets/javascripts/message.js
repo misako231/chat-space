5e1a0002e01669544d69a58287e18ee98789b95d
misako231 <melon_pal.231@icloud.com>
2019-01-11 12:41:37 +0900

    create message ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,52 @@
+$(function() {
+  function buildHTML(message) {
+    var newContent = '';
+      if (message.content) {
+       newContent = `<p class="lower-message__content">${ message.content }</p>`;
+      }
+    var newImage = '';
+      if (message.image) {
+        newImage = `<img class="lower-message__image" src="${ message.image }">`;
+      }
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user">
+                      ${ message.user_name }
+                    </div>
+                    <div class="upper-message__date">
+                      ${ message.created_at }
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    ${ newContent }
+                    ${ newImage }
+                  </div>
+                </div>`
+    return html;
+  }
+
+  $('.new_message').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.main__messages').append(html);
+      $('.message-box').val('');
+      // テキストフィールドをからにする
+      $('.main__messages').animate({ scrollTop: $('.main__messages')[0].scrollHeight});
+      $('.send').prop('disabled', false);
+    })
+    .fail(function(data) {
+      alert('error');
+    })
+  });
+});
