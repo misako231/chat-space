76ae159193e95e169038150329e20861faeb2252
misako231 <melon_pal.231@icloud.com>
2019-01-13 18:54:46 +0900

    add buildHTML function

--- app/assets/javascripts/update.js
+++ app/assets/javascripts/update.js
@@ -1,4 +1,32 @@
 $(function() {
+  function buildHTML(message) {
+    var sendImage = '';
+    var sendContent='';
+    if (message.image.url) {
+      sendImage = `<img src="${message.image.url}" class="lower-message__image">`;
+    }
+    if (message.content) {
+      sendContent = `<p class="lower-message__content">
+                      ${message.content}
+                    </p>`
+    }
+
+    var html =`<div class="message" data-message-id="${message.id}">
+                <div class="upper-message">
+                  <div class="upper-message__user">
+                    ${message.name}
+                  </div>
+                  <div class="upper-message__date">
+                    ${message.date}
+                  </div>
+                </div>
+                <div class="lower-message">
+                    ${sendImage}
+                    ${sendContent}
+                </div>
+              </div>`
+    return html
+  }
 
   function update() {
     $.ajax({
@@ -7,7 +35,12 @@ $(function() {
       dataType: 'json'
     })
     .done(function(messages) {
-
+     var id = $('.message').data('message-id');
+      messages.forEach(function(message) {
+        if (message.id > id) {
+          $('.main__messages').append(buildHTML(message));
+        }
+      });
     })
     .fail(function(data) {
       alert('自動更新に失敗しました');
