8d8bcd4e7751c0d6d4a26d644e4eefc06bfe2fc8
misako231 <melon_pal.231@icloud.com>
2019-01-13 18:52:30 +0900

    add ajax and update function

--- app/assets/javascripts/update.js
+++ app/assets/javascripts/update.js
@@ -1,5 +1,20 @@
 $(function() {
-  setInterval(function() {
 
-  },5000);
+  function update() {
+    $.ajax({
+      type: 'GET',
+      url: location.href,
+      dataType: 'json'
+    })
+    .done(function(messages) {
+
+    })
+    .fail(function(data) {
+      alert('自動更新に失敗しました');
+    });
+  }
+
+  $(function() {
+    setInterval(update, 5000);
+  });
 });
